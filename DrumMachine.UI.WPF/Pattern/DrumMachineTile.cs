3fabba89c3bb55ca5e63a1bba50225c815d615b0
dkijania <dariusz.kijania@gmail.com>
2015-07-30 20:42:35 +0200

    commiting leftovers

---
+++ DrumMachine.UI.WPF/Pattern/DrumMachineTile.cs
@@ -0,0 +1,38 @@
+﻿namespace DrumMachine.UI.WPF.Pattern
+{
+    public class DrumMachineTile
+    {
+        public int Row { get; protected set; }
+        public int Column { get; protected set; }
+        public event OnSelect OnSelectEvent;
+        public event IgnoreMouseClick OnIgnoreMouseClick;
+
+        public virtual bool InvokeOnIgnoreMouseClick()
+        {
+            IgnoreMouseClick handler = OnIgnoreMouseClick;
+            return handler != null && handler();
+        }
+
+        //check if function is valid
+        public virtual void InvokeOnSelectEvent(int row, int column, bool isSelected)
+        {
+            var handler = OnSelectEvent;
+            if (handler != null)
+             handler(row,column,isSelected);
+            
+        }
+
+        public delegate void OnSelect(int row, int column,bool isSelected);
+        public delegate bool IgnoreMouseClick();
+
+
+        public DrumMachineTile(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+
+    }
+}
+    
---
+++ DrumMachine/Audio/DefaultAudioSampler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrumMachine.Engine.Sample;
+
+namespace DrumMachine.Audio
+{
+    public class DefaultAudioSampler
+    {
+        private static DefaultAudioSampler _instance;
+
+        public static DefaultAudioSampler Instance
+        {
+            get { return _instance ?? (_instance = new DefaultAudioSampler()); }
+        }
+
+        private readonly Dictionary<String, SampleSource> _dictionary = new Dictionary<string, SampleSource>();
+
+        private DefaultAudioSampler()
+        {
+            _dictionary.Add("Kick", SampleSource.CreateFromWaveFile("Audio\\Samples\\kick-trimmed.wav"));
+            _dictionary.Add("Snare", SampleSource.CreateFromWaveFile("Audio\\Samples\\snare-trimmed.wav"));
+            _dictionary.Add("Closed Hat", SampleSource.CreateFromWaveFile("Audio\\Samples\\closed-hat-trimmed.wav"));
+            _dictionary.Add("Open Hat", SampleSource.CreateFromWaveFile("Audio\\Samples\\open-hat-trimmed.wav"));
+            _dictionary.Add("Crash", SampleSource.CreateFromWaveFile("Audio\\Samples\\crash-trimmed.wav"));
+            _dictionary.Add("Hi-mid Tom", SampleSource.CreateFromWaveFile("Audio\\Samples\\Hi-Mid Tom.wav"));
+            _dictionary.Add("High Floor Tom", SampleSource.CreateFromWaveFile("Audio\\Samples\\High Floor Tom.wav"));
+            _dictionary.Add("Low Floor Tom", SampleSource.CreateFromWaveFile("Audio\\Samples\\Low Floor Tom.wav"));
+            _dictionary.Add("High Tom", SampleSource.CreateFromWaveFile("Audio\\Samples\\High Floor Tom.wav"));
+            _dictionary.Add("Low Tom", SampleSource.CreateFromWaveFile("Audio\\Samples\\Low Tom.wav"));
+            _dictionary.Add("Low-mid Tom", SampleSource.CreateFromWaveFile("Audio\\Samples\\Low-Mid Tom.wav"));
+        }
+
+
+        public IEnumerable<String> Names
+        {
+            get { return _dictionary.Keys; }
+        }
+
+        public IEnumerable<SampleSource> Samples
+        {
+            get { return _dictionary.Values; }
+        }
+
+        public SampleSource this[string key]
+        {
+            get { return _dictionary[key]; }
+        }
+    }
+}
