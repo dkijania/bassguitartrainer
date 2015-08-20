45dbe410d82559b89c491b6eae27016f23f67169
dkijania <dariusz.kijania@gmail.com>
2015-08-20 22:24:13 +0200

    fixing problems with undefined variable in wix project. Commiting leftovers

---
+++ BassTrainer.Core/Const/BassGuitar/Parameters/Tuning/TuningSounds.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BassTrainer.Core.Const.BassGuitar.Parameters.Tuning
+{
+    public class TuningSounds
+    {
+        public BassGuitarTuningId TuningId { get; private set; }
+        public string Name { get; private set; }
+        public IEnumerable<Note> Notes { get; private set; }
+        private LinkedList<Note> OrderedList { get; set; }
+
+        public TuningSounds(string name, params Note[] sounds)
+        {
+            Name = name;
+            Notes = sounds;
+            OrderedList = new LinkedList<Note>(Notes);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", Name, String.Join("-", Notes));
+        }
+
+        public Note GetNextTo(Note note)
+        {
+            var foundNote = GetNode(note);
+            return IsLast(foundNote) ? OrderedList.First() : foundNote.Next.Value;
+        }
+
+        public Note GetPrevTo(Note note)
+        {
+            var foundNote = GetNode(note);
+            return IsPrev(foundNote) ? OrderedList.Last() : foundNote.Previous.Value;
+        }
+
+        private LinkedListNode<Note> GetNode(Note note)
+        {
+            var foundNote = OrderedList.Find(note);
+            CheckIfNull(foundNote, note);
+            return foundNote;
+        }
+
+        private void CheckIfNull(LinkedListNode<Note> node, Note note)
+        {
+            if (node == null)
+                throw new InvalidDataException(String.Format("Note: {0} should be in collection: {1}", note, Notes));
+        }
+
+        private bool IsLast(LinkedListNode<Note> node)
+        {
+            return node.Next == null;
+        }
+
+        private bool IsPrev(LinkedListNode<Note> node)
+        {
+            return node.Previous == null;
+        }
+    }
+}
---
+++ WavPlayer/LoopStream.cs
@@ -0,0 +1,73 @@
+﻿using NAudio.Wave;
+
+namespace WavPlayer
+{
+    public class LoopStream : WaveStream
+    {
+        private readonly WaveStream _sourceStream;
+
+        public event OnLoopEndEvent OnLoopEnd;
+        public delegate void OnLoopEndEvent();
+
+        protected virtual void InvokeOnLoopEnd()
+        {
+            OnLoopEndEvent handler = OnLoopEnd;
+            if (handler != null) handler();
+        }
+
+        public LoopStream(WaveStream sourceStream)
+        {
+            this._sourceStream = sourceStream;
+            this.EnableLooping = true;
+        }
+
+        public bool EnableLooping { get; set; }
+
+        public override WaveFormat WaveFormat
+        {
+            get { return _sourceStream.WaveFormat; }
+        }
+
+        public override long Length
+        {
+            get { return _sourceStream.Length; }
+        }
+
+        public override long Position
+        {
+            get { return _sourceStream.Position; }
+            set { _sourceStream.Position = value; }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var totalBytesRead = 0;
+
+            while (totalBytesRead < count)
+            {
+                var bytesRead = _sourceStream.Read(buffer, offset + totalBytesRead, count - totalBytesRead);
+                if (bytesRead == 0)
+                {
+                    if (_sourceStream.Position == 0 || !EnableLooping)
+                    {
+                        break;
+                    }
+                    ResetPosition();
+                    FireOnLoopEndEvent();
+                }
+                totalBytesRead += bytesRead;
+            }
+            return totalBytesRead;
+        }
+
+        private void ResetPosition()
+        {
+            _sourceStream.Position = 0;
+        }
+
+        private void FireOnLoopEndEvent()
+        {
+            InvokeOnLoopEnd();
+        }
+    }
+}
